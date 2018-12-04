d7254944c26a030d2a51e4f466a9991e9e75bf38
Odos Matthews <odosmatthews.gmail.com>
2018-12-03 20:28:27 -0500

    got it to add

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,12 +1,18 @@
 package org.launchcode.controllers;
 
 import org.launchcode.models.Category;
+import org.launchcode.models.Cheese;
 import org.launchcode.models.data.CategoryDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "category")
@@ -30,7 +36,9 @@ public class CategoryController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add() {
-        return "";
+    public String add(@ModelAttribute @Valid Category category,
+                      Errors errors, Model model) {
+        categoryDAO.save(category);
+        return "redirect:";
     }
 }
