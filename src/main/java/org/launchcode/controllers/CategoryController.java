b5835ccf01fd92457f8c16b3432da98f6471eb57
Odos Matthews <odosmatthews.gmail.com>
2018-12-03 19:58:35 -0500

    added Category model and controller

---
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -0,0 +1,33 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.data.CategoryDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "category")
+public class CategoryController {
+    @Autowired
+    private CategoryDAO categoryDAO;
+
+    @RequestMapping(value = "")
+    public String index(Model viewBag) {
+
+        viewBag.addAttribute("title", "All Categories");
+        viewBag.addAttribute("categories", categoryDAO.findAll());
+        return "category/index";
+    }
+
+    @RequestMapping(value = "add")
+    public String add() {
+        return "category/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model viewBag) {
+        return "";
+    }
+}
